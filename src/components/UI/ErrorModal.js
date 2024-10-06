13db2ecea0cd70c88af10aeb2228841545c78dee
AnanSer <ananserbesa@gmail.com>
2024-10-07 00:44:35 +0300

    added errorModal with styling

--- src/components/UI/ErrorModal.js
+++ src/components/UI/ErrorModal.js
@@ -1,6 +1,20 @@
 import React from "react";
 import Card from "./Card";
+import Button from "./Button";
+import classes from "./ErrorModal.module.css";
 
-const ErrorModal = (props) => {};
+const ErrorModal = (props) => {
+  <Card className={classes.modal}>
+    <header className={classes.header}>
+      <h2>props.title</h2>
+    </header>
+    <div className={classes.content}>
+      <p>props.message</p>
+    </div>
+    <footer className={classes.action}>
+      <Button>ok</Button>
+    </footer>
+  </Card>;
+};
 
 export default ErrorModal;
